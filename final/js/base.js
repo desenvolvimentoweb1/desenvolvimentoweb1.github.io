54a60c9f10a18d75d66a70976bf216775f2889b4
iago-costa <univercomput@gmail.com>
2022-11-06 16:59:14 -0300

    update base.js

--- final/js/base.js
+++ final/js/base.js
@@ -1,4 +1,9 @@
 
 
 
-document.getElementById("id__footer__title").innerHTML = "Trabalho final WEB I<br>Alunos: Iago Costa, Kayro Santos e Leyrisvan";
+document.getElementById("id__footer__title").innerHTML = "Trabalho final WEB I<br>Alunos: Iago Costa, Kayro Santos e Leyrisvan da Costa<br>Professor: Warley<br>2020.2";
+
+
+// Trabalho final: https://desenvolvimentoweb1.github.io/final/inicio.html
+// Código fonte: https://github.com/desenvolvimentoweb1/desenvolvimentoweb1.github.io/tree/main/final
+// Alunos: Iago Costa, Kayro Santos e Leyrisvan da Costa
